a23ac2e56451a9b960d149db20e8773cb6184f21
MostafaMahmoud97 <mostafamahmoud111115@gmail.com>
2023-12-16 15:49:51 +0200

    change in leadbank table

--- database/migrations/2023_11_11_044422_create_leads_bank_table.php
+++ database/migrations/2023_11_11_044422_create_leads_bank_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('leads_bank', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->enum('customer_type',['residential','commercial']);
             $table->string('first_name');
             $table->string('last_name');
